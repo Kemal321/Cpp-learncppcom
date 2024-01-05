9d39a00d6b87f03231f6cbf0ab9bd5567bc0f361
Kemalettin KARA <62467910+Kemal321@users.noreply.github.com>
2024-01-05 22:06:02 +0300

    Add files via upload

---
+++ Exercises/Exercism C++/Difference Of Squares/differenceOfSquares.h
@@ -0,0 +1,10 @@
+#if !defined(DIFFERENCE_OF_SQUARES_H)
+#define DIFFERENCE_OF_SQUARES_H
+
+namespace difference_of_squares {
+    int square_of_sum(int counter);
+    int sum_of_squares(int counter);
+    int difference(int counter);
+}  // namespace difference_of_squares
+
+#endif // DIFFERENCE_OF_SQUARES_H
