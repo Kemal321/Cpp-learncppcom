b72a84115031f601b06c9901045be6686d69fb11
Kemalettin KARA <62467910+Kemal321@users.noreply.github.com>
2024-01-18 21:46:32 +0300

    Add files via upload

---
+++ Exercises/Exercism C++/Reverse String/reverse_string .h
@@ -0,0 +1,8 @@
+#if !defined(REVERSE_STRING_H)
+#define REVERSE_STRING_H
+#include <string>
+namespace reverse_string {
+std::string reverse_string(std::string reversable);
+}  // namespace reverse_string
+
+#endif // REVERSE_STRING_H
